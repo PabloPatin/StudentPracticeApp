c60427de64985a6f375dc21f9d1669cbc71d3ca3
PabloPatin <pashka.lop@mail.ru>
2024-11-30 14:58:33 +0300

    start project

---
+++ src/main/java/com/app/StudentPracticeApp/StudentPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.StudentPracticeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/StudentPracticeApp/StudentPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.StudentPracticeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
