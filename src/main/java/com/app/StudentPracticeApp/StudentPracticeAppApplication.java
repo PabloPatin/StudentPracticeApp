eb5acdc3ce8350fc7853a3203bb1446b867dfa04
PabloPatin <pashka.lop@mail.ru>
2024-12-01 00:33:29 +0300

    Start frontend

--- src/main/java/com/app/StudentPracticeApp/StudentPracticeAppApplication.java
+++ src/main/java/com/app/StudentPracticeApp/StudentPracticeAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentPracticeAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StudentPracticeAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPracticeAppApplication.class, args);
+    }
 }
